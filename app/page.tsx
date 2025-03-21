bab6da8cbc619a7a42350c7593c25b25207cca6a
Collins <149684399+coll254-strath@users.noreply.github.com>
2025-03-21 22:09:53 +0000

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 
-import CallToAction from "@/components/call-to-action";
 import FeaturesSection from "@/components/features-5";
 import HeroSection from "@/components/hero-section";
 import StatsSection from "@/components/stats";
 import TestimonialsSection from "@/components/testimonials";
-
 import WallofLoveSection from "@/components/testimonials";
 import CallToActionSection from "@/components/call-to-action";
 import Pricing from "@/components/pricing";
